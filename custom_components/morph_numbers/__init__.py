87a39d01a0cda9ac8a69cb151c7e793a1fabae7c
Alexey Khit <alexey.khit@gmail.com>
2021-05-14 12:13:48 +0300

    Adds config via GUI

--- custom_components/morph_numbers/__init__.py
+++ custom_components/morph_numbers/__init__.py
@@ -1,7 +1,34 @@
+from homeassistant.config_entries import SOURCE_IMPORT
 from homeassistant.helpers.template import _ENVIRONMENT, TemplateEnvironment
 from jinja2.filters import do_format
 
 from . import utils
+
+DOMAIN = 'morph_numbers'
+
+
+# noinspection PyUnusedLocal
+async def async_setup(hass, hass_config):
+    if DOMAIN in hass_config:
+        hass.async_create_task(hass.config_entries.flow.async_init(
+            DOMAIN, context={"source": SOURCE_IMPORT}
+        ))
+    return True
+
+
+async def async_setup_entry(hass, entry):
+    if _ENVIRONMENT not in hass.data:
+        hass.data[_ENVIRONMENT] = TemplateEnvironment(hass)
+
+    # use an existing filter so as not to get an initialization error
+    hass.data[_ENVIRONMENT].filters['format'] = morph_format
+
+    return True
+
+
+async def async_unload_entry(hass, entry):
+    hass.data[_ENVIRONMENT].filters['format'] = do_format
+    return True
 
 
 def morph_format(value, *args, **kwargs):
@@ -10,15 +37,3 @@ def morph_format(value, *args, **kwargs):
         return utils.numword(value, kwargs['morph'], as_text)
     else:
         return do_format(value, *args, **kwargs)
-
-
-# noinspection PyUnusedLocal
-async def async_setup(hass, hass_config):
-    if _ENVIRONMENT not in hass.data:
-        hass.data[_ENVIRONMENT] = TemplateEnvironment(hass)
-
-    # используем уже существующий фильтр, чтоб не нарваться на ошибку
-    # инициализации
-    hass.data[_ENVIRONMENT].filters['format'] = morph_format
-
-    return True
